80acdc09608a0e5478124955607f7aa9c1514e4f
visionvlsi <98731221+visionvlsi@users.noreply.github.com>
2022-05-23 04:22:50 +0530

    Update gates_usingMux.v

--- VerilogHDL/Assignment_0/gates_usingMux.v
+++ VerilogHDL/Assignment_0/gates_usingMux.v
@@ -87,3 +87,49 @@ module test;
       $monitor("simtime=%t,a=%b,b=%b,y=%b",$time,a,b,y);
 endmodule
 
+
+//_______________________________________________________________
+//______________________________XOR Gate using Mux21______________
+//_________________________________________________________________
+   
+//XOR gate using mux
+/*
+   | A | B | Y |
+   ______________
+   | 0 | 0 | 0 |
+   ______________
+   | 0 | 1 | 1 |
+   ______________
+   | 1 | 0 | 1 |
+    ______________
+   | 1 | 1 | 0 |
+   ______________
+
+*/
+
+//design code
+module xorgate_UsingMUx(a,b,y);
+   input a,b;
+   output y;
+  assign y=b?1'b1:1'b0;
+endmodule
+
+
+`timescale 1ns/1ns
+module test;
+   reg a,b;
+   wire y;
+   xorgate_UsingMUx ins1(a,b,y);
+   initial begin
+      a=0; b=0;
+      #5 b=1;
+      #5 a=1;
+      #5 b=0;
+      #5 $finish;
+   end
+   
+   initial
+      $monitor("simtime=%t,a=%b,b=%b,y=%b",$time,a,b,y);
+endmodule
+
+
