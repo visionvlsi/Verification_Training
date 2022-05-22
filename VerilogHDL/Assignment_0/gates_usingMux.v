44eeeffd32909a006cd90ce14652aff9602a1239
visionvlsi <98731221+visionvlsi@users.noreply.github.com>
2022-05-22 22:47:39 +0530

    Update gates_usingMux.v

--- VerilogHDL/Assignment_0/gates_usingMux.v
+++ VerilogHDL/Assignment_0/gates_usingMux.v
@@ -1,17 +1,17 @@
 //Design and implement Or, And, Xor gate using Multiplexer (2×1).
 
 //or gate using mux
-
-// | A | B |  Y |
+/*
+   | A | B | Y |
    ______________
-   | 0  | 0 | 0 |
+   | 0 | 0 | 0 |
    ______________
-   | 0  | 1 | 1 |
+   | 0 | 1 | 1 |
    ______________
-   | 0  | 0 | 0 |
+   | 1 | 0 | 1 |
     ______________
-   | 0  | 0 | 0 |
+   | 1 | 1 | 1 |
    ______________
 
+*/
 
-https://www.edaplayground.com/x/Bz48
