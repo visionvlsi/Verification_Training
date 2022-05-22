b8671d4f1c0977a8942c4e9da0589b07c26debf0
visionvlsi <98731221+visionvlsi@users.noreply.github.com>
2022-05-23 04:16:54 +0530

    Update gates_usingMux.v

--- VerilogHDL/Assignment_0/gates_usingMux.v
+++ VerilogHDL/Assignment_0/gates_usingMux.v
@@ -42,4 +42,48 @@ module test;
 endmodule
    
 // EDAPlayground link https://www.edaplayground.com/x/fX5M
+
+//_______________________________________________________________
+//______________________________AND Gate using Mux21______________
+//_________________________________________________________________
    
+//AND gate using mux
+/*
+   | A | B | Y |
+   ______________
+   | 0 | 0 | 0 |
+   ______________
+   | 0 | 1 | 0 |
+   ______________
+   | 1 | 0 | 0 |
+    ______________
+   | 1 | 1 | 1 |
+   ______________
+
+*/
+
+//design code
+module andgate_UsingMUx(a,b,y);
+   input a,b;
+   output y;
+  assign y=b?(a?1'b1:1'b0):1'b0;
+endmodule
+
+
+`timescale 1ns/1ns
+module test;
+   reg a,b;
+   wire y;
+   andgate_UsingMUx ins1(a,b,y);
+   initial begin
+      a=0; b=0;
+      #5 b=1;
+      #5 a=1;
+      #5 b=0;
+      #5 $finish;
+   end
+   
+   initial
+      $monitor("simtime=%t,a=%b,b=%b,y=%b",$time,a,b,y);
+endmodule
+
