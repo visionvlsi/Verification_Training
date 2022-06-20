26ecce3a724e45b017ea1a8083c78e61357d927b
visionvlsi <98731221+visionvlsi@users.noreply.github.com>
2022-06-21 03:24:45 +0530

    Create level3_3.v

---
+++ VerilogHDL/Level-3/level3_3.v
@@ -0,0 +1,6 @@
+/*
+7. write a program for synchronous fifo for 32 locations depth and 16 bit data size?
+
+8. design and implement asynchronous fifo with write clock 512 mhz and read clock 64 mhz?(hint: calculate the fifo depth and proceed)
+
+*/
