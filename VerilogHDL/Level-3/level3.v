99fb70cd761f1239f10a36ebba444c8856633132
visionvlsi <98731221+visionvlsi@users.noreply.github.com>
2022-06-21 01:56:36 +0530

    Update level3.v

--- VerilogHDL/Level-3/level3.v
+++ VerilogHDL/Level-3/level3.v
@@ -81,3 +81,87 @@ module tb;
 endmodule
 
 ///https://www.edaplayground.com/x/H5WG
+
+
+/////////shifter 
+
+module shift_reg   (d, clk,dir,rst,out);    
+  input d,clk,dir,rst;
+  output reg out;
+  
+  reg [7:0]int_reg;
+  
+  
+   always @ (posedge clk)  
+     begin
+     if (rst)  
+         int_reg = 0;  
+      else   
+            case (dir)  
+              0 : begin int_reg = {int_reg[6:0], d};
+                out=int_reg[7];
+                   end//left shift
+              1 :  begin int_reg = {d, int_reg[7:1]};
+                out=int_reg[0];
+                   end//right shift
+            endcase  
+              
+     end
+endmodule  
+
+//
+// Code your testbench here
+// or browse Examples
+`timescale 1ns/1ps
+module tb_sr;  
+  
+   reg d;               
+   reg clk;                 
+   reg dir;                   
+   reg rst;                
+   wire out;     
+  
+  shift_reg   ins1(d, clk,dir,rst,out); 
+  
+   // Generate clock time period = 20ns, freq => 50MHz  
+   always #10 clk <= ~clk;  
+  
+   // Initialize variables to default values at time 0  
+   initial begin  
+      clk <= 0;  
+      dir <= 0;  
+       d <= 1;  
+   end  
+  
+   // Drive main stimulus to the design to verify if this works  
+   initial begin  
+  
+      // 1. Apply reset and deassert reset after some time  
+     rst <= 0;  
+      #10 rst <= 1;  
+      #2 rst <= 0;  
+       
+  
+      // 2. For 7 clocks, drive alternate values to data pin  
+      repeat (7) @ (posedge clk)  
+         d <= ~d;  
+  
+     // 3. Shift direction and drive alternate value to data pin for another 7 clocks  
+      #10 dir <= 1;  
+      repeat (7) @ (posedge clk)  
+         d <= ~d;  
+  
+      // 4. Drive nothing for the next 7 clocks, allow shift register to shift based on dir simply  
+      repeat (7) @ (posedge clk);  
+  
+      // 5. Finish the simulation  
+      $finish;  
+   end  
+  
+   // Monitor values of these variables and print them into the log file for debug  
+   initial  
+     $monitor ($time, " clk=%b, rst=%0b d=%b,  dir=%0b, out=%b", clk,rst, d, dir, out);  
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
+endmodule  
