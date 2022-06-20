9f37d2560304c6549afd3b131323cda6226bc4ec
visionvlsi <98731221+visionvlsi@users.noreply.github.com>
2022-06-20 19:48:37 +0530

    Create level3.v

---
+++ VerilogHDL/Level-3/level3.v
@@ -0,0 +1,26 @@
+/*LEVEL-3:
+
+1. Write the hardware description of a 4-bit PRBS (pseudo-random Binary sequence) generator using a linear feedback shift register and test it.?
+
+2. Write the hardware description of a 8-bit register with shift left and shift right modes of operation based on selection line and  write its test operation?
+
+3. Write a program for universal shift register?
+
+4. write a program for universal synchronous counter which includes up , down and updown counter?
+
+5. write a program for universal asynchronous counter which includes up , down and updown counter?
+
+6. Write the hardware description of a 4-bit mod-13 counter and test it.?
+
+7. write a program for synchronous fifo for 32 locations depth and 16 bit data size?
+
+8. design and implement asynchronous fifo with write clock 512 mhz and read clock 64 mhz?(hint: calculate the fifo depth and proceed) */
+
+////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+//1. Write the hardware description of a 4-bit PRBS (pseudo-random Binary sequence) generator using a linear feedback shift register and test it.?
+
+/* PRBS or Pseudo Random Binary Sequence is essentially a random sequence of binary numbers. It is random in a sense that the value of an element of the 
+sequence is independent of the values of any of the other elements. It is 'pseudo' because it is deterministic and after N elements it starts to repeat itself, 
+unlike real random sequences.. */
