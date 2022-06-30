a7e84c9163683348cd54d5a36eebf5ff58fa330d
visionvlsi <98731221+visionvlsi@users.noreply.github.com>
2022-06-30 14:53:40 +0530

    Create pending_assignments1.v

---
+++ VerilogHDL/pending_assignments1.v
@@ -0,0 +1,32 @@
+// ANALYSE THE DFF WAVEFORMS FOR BOTH SYNCHRONOUS AND ASYNCHRONOUS CIRCUITS.
+
+
+
+
+
+
+
+// 1. Synchronous Counters : Based on clock
+ 
+
+
+
+
+
+
+
+
+
+//2. Asynchronous Counters: May be clock or more than signal other than clock
+
+
+//3. Structural or Behavioral Counters Count 0-10 : Async 4 FF
+
+
+// 4. Up and Down Counter - 0 1 2 3 2 1 0 1 2 3 2 1 0  .....
+   
+
+// 5. 0123454323456789876 - TRY
+
+
+//6. FSM TYPE COUNTERS 1. ModN Counter 2.   Mixed Mod 3.Fancy
