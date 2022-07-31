c0e159a1112cecf92887b095e88f20243ca39a04
Vision-VLSI Industry PVT. LTD <vvi.pvtltd@gmail.com>
2022-07-31 09:30:36 +0530

    Update q4_alu.sv

--- systemverilog/q4_alu.sv
+++ systemverilog/q4_alu.sv
@@ -18,19 +18,19 @@ class alu;
     case(sel)
         0: begin 
               this.result= this.operand1 + this.operand2; // add
-             $display(this.result);
+          $display(" For the operands %0d, %0d and for the sel=%0d,The value of addition is : %0d",this.operand1, this.operand2, this.sel,this.result);
             end
         1: begin 
             this.result= this.operand1 - this.operand2; // sub
-            $display(this.result);
+          $display(" For the operands %0d, %0d and for the sel=%0d,The value of subtraction is %0d",this.operand1, this.operand2, this.sel,this.result);
             end
         2: begin 
             this.result= this.operand1 * this.operand2; // mul
-            $display(this.result);
+          $display(" For the operands %0d, %0d and for the sel=%0d,The value of multiplication is : %0d",this.operand1, this.operand2, this.sel,this.result);
             end
         3: begin 
             this.result= this.operand1 / this.operand2; // div
-            $display(this.result);
+          $display(" For the operands %0d, %0d and for the sel=%0d,The value of division is: %0d",this.operand1, this.operand2, this.sel,this.result);
             end
     endcase
     
@@ -45,11 +45,16 @@ module tb;
   bit [8:0]result;
   alu al;
     initial begin
+      operand1=10;
+      operand2=5;
       
-      al=new(10,5,0,result);
-      
+      for(int i=0; i<4; i++) begin
+        sel=i;      
+        al=new(operand1,operand2,sel,result);
+        #5;      
+    end
     end
   
 endmodule
 
-//https://www.edaplayground.com/x/v5XM
+//https://www.edaplayground.com/x/r99t
